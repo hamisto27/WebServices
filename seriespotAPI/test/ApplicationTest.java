a8ee00238cf9024cca508be11964185d57f7f266
hamisto27 <clientx27@gmail.com>
2014-04-30 11:22:06 +0200

    fuck test

--- seriespotAPI/test/ApplicationTest.java
+++ seriespotAPI/test/ApplicationTest.java
@@ -2,7 +2,6 @@
 *
 * Simple (JUnit) tests that can call all parts of a play application.
 * If you are interested in mocking a whole application, see the wiki for more details.
-*dzfjsdkkfjksdf
 */
 public class ApplicationTest {
 
